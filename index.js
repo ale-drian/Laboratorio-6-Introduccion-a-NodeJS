49fd4f8ea037039245e1d2df086c1e5633b954f2
unknown <alejandraadriantejada@gmail.com>
2021-04-22 19:37:08 -0500

    listar

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const port = 3001;
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-1234"
+    },
+    {
+        id: 2,
+        name: "Mary Hellas",
+        number: "51-1234"
+    },
+    {
+        id: 3,
+        name: "Nora Jules",
+        number: "040-6547"
+    },
+    {
+        id: 4,
+        name: "Juan Males",
+        number: "041-2020"
+    },
+    {
+        id: 5,
+        name: "Lola Ruiz",
+        number: "123-1234"
+    }
+]
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World');
+  response.setHeader('Content-Type', 'application/json');
+  response.end(JSON.stringify(persons));
 });
 
 /*server.listen(port, hostname, () => {
