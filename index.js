5b0f8cb4731b511ee40c637a283f6cb8debcef0e
unknown <alejandraadriantejada@gmail.com>
2021-04-24 21:13:12 -0500

    eliminar elemento

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+//Eliminar un elemento
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
